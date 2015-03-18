b4cb7a04ba3f399a638b1245d7c87b1d36d08040
Christian Eichhorn <c.eichhorn@webmasters.de>
2015-03-18 01:42:21 +0100

    * profitbricks server cook    * improved

--- lib/chef/knife/profitbricks_server_cook.rb
+++ lib/chef/knife/profitbricks_server_cook.rb
@@ -3,6 +3,19 @@ require 'knife_profitbricks_fog/base'
 module KnifeProfitbricksFog
   class ProfitbricksServerCook < Chef::Knife
     include KnifeProfitbricksFog::Base
+      
+    deps do
+      require 'net/ssh'
+      require 'net/ssh/multi'
+      
+      require 'chef/mixin/command'
+      require 'chef/knife/solo_bootstrap'
+      require 'chef/knife/solo_cook'
+      
+      require 'securerandom'
+      require 'timeout'
+      require 'socket'
+    end
 
     banner "knife profitbricks server cook OPTIONS"
 
