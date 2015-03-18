f52a53a9ceaa3ebfcb6b309967e7ca9bd8f347a5
Christian Eichhorn <c.eichhorn@webmasters.de>
2015-03-18 14:05:54 +0100

    * server list improved

--- lib/chef/knife/profitbricks_server_list.rb
+++ lib/chef/knife/profitbricks_server_list.rb
@@ -7,13 +7,14 @@ module KnifeProfitbricksFog
     banner "knife profitbricks server list OPTIONS"
 
     def run
-      compute.datacenters.each do |dc|
+      datacenters.each do |dc|
         log "DC: #{dc.name}"
 
-        compute.servers.all.select {|s| s.data_center_id == dc.id }.each do |server|
+        servers_for_datacenter(dc).each do |server|
           log " * Server: #{server.name} (#{server.cores} cores; #{server.ram} MB RAM; IP: #{server.interfaces.first.ips}; #{server.machine_state})"
 
-          compute.volumes.all.select {|s| s.server_ids == server.id }.each do |volume|
+
+          volumes_for_server(server).each do |volume|
             log "   * Volume: #{volume.name} (#{volume.size} GB)"
           end
           
@@ -27,5 +28,43 @@ module KnifeProfitbricksFog
       @data_centers ||= compute.datacenters.all
     end
     alias dcs data_centers
+
+    def datacenters
+      @datacenters ||= compute.datacenters.all
+    end
+
+    def servers
+      @servers ||= compute.servers.all
+    end
+
+    def volumes
+      @volumes ||= compute.volumes.all
+    end
+
+    def servers_for_dc(dc)
+      @servers_by_dc ||= servers.inject({}) do |sum, server|
+        sum[server.data_center_id] ||= []
+        sum[server.data_center_id] << server
+        sum
+      end
+
+      @servers_by_dc[dc.id]
+    end
+    alias servers_for_datacenter servers_for_dc
+
+    def volumes_for_server(server)
+      @volumes_by_dc ||= volumes.inject({}) do |sum, volume|
+        sum[volume.server_ids] ||= []
+        sum[volume.server_ids] << volume
+        sum
+      end
+
+      avs__device_numbers = server.attached_volumes.inject({}) do |sum, v|
+        sum[v['id']] = v['device_number']
+        sum
+      end
+
+      @volumes_by_dc[server.id].sort_by {|v| avs__device_numbers[v.id] }
+    end
   end
 end
