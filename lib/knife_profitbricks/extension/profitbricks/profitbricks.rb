8fb394fbeb422436048f9f54674e2ed622f03a6d
Christian Eichhorn <c.eichhorn@webmasters.de>
2015-08-31 01:58:35 +0200

    * debugging output removed

--- lib/knife_profitbricks/extension/profitbricks/profitbricks.rb
+++ lib/knife_profitbricks/extension/profitbricks/profitbricks.rb
@@ -11,16 +11,11 @@ module KnifeProfitbricks
         end
 
         def request_with_set_request_id_to_model(*args)
-          p caller[0..4]
           caller_binding = binding.eval('self') # get object which called this method
-          p caller_binding
           response = request_without_set_request_id_to_model(*args)
           
           if caller_binding.respond_to? :requestId
-            p "request for: #{caller_binding.class}"
-            p "old: #{caller_binding.requestId}"
             caller_binding.requestId = (response[:requestId] || response['requestId'])
-            p "new: #{caller_binding.requestId}"
           end
 
           response
