6117656be4c83c50d669a090c0b36acfa8065694
Christian Eichhorn <c.eichhorn@webmasters.de>
2015-12-09 10:55:42 +0100

    * KnifeProfitbricks::Extension::Profitbricks::HasLocation    * improved

--- lib/knife_profitbricks/extension/profitbricks/has_location.rb
+++ lib/knife_profitbricks/extension/profitbricks/has_location.rb
@@ -4,16 +4,20 @@ module KnifeProfitbricks
       module HasLocation
         def self.included(base)
           base.class_eval do
-            property_reader :location
+#            property_reader :location
           end
         end
 
-        def _location
-          @location ||= ProfitBricks::Location.by_id(location)
+        def location_id
+          read_property :location
+        end
+
+        def location
+          @location ||= ProfitBricks::Location.by_id location_id
         end
 
         def location_label
-          _location.label
+          location.label
         end
       end
     end
