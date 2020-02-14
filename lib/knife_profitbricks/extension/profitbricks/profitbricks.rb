e17462a37e54c81107b057ae7a0d2890fc8b918c
Christian Eichhorn <c.eichhorn@webmasters.de>
2020-02-14 08:51:17 +0100

    * KnifeProfitbricks::::Extension::Profitbricks     * rate limit fixed

--- lib/knife_profitbricks/extension/profitbricks/profitbricks.rb
+++ lib/knife_profitbricks/extension/profitbricks/profitbricks.rb
@@ -11,7 +11,27 @@ module KnifeProfitbricks
         end
 
         def request_with_set_request_id_to_model(options)
-          response = request_without_set_request_id_to_model(options)
+          retry_count = 0
+          max_retry_count = 50
+
+          response = begin
+            request_without_set_request_id_to_model options
+          rescue JSON::ParserError => e
+            if e.message.match?(/Rate\s*Limit\s*Exceeded/i) && retry_count <= max_retry_count
+              divider = "*" * 20 + "\n"
+              print divider
+              print e.message
+              print divider
+              print "Wait ... #{retry_count}/#{max_retry_count}\n"
+              print divider
+              retry_count += 1
+              sleep 20
+              retry
+            else
+              raise e
+            end
+          end
+
           request_id = (response[:requestId] || response['requestId'])
          
           if options[:method].to_sym != :get && request_id
