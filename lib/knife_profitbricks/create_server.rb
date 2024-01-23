e54f7076a8cdf64e5d0ef8f78591d0359c9f2113
Christian Eichhorn <c.eichhorn@webmasters.de>
2024-01-23 09:12:13 +0100

    create server fixed

--- lib/knife_profitbricks/create_server.rb
+++ lib/knife_profitbricks/create_server.rb
@@ -2,57 +2,6 @@ module KnifeProfitbricks
   module CreateServer
 
     private
-    def create_volumes
-      unless configured_volumes = server_config['volumes']
-        error("No volumes specified! Please specify \"profitbricks\": {\"server\": \"volumes\": {\"root\": SIZE_IN_GB}} in your node!")
-      end
-
-      threads = configured_volumes.collect do |hd_name, size_in_gb|
-        if size_in_gb.is_a? Hash
-          _thread_for_create_volume hd_name, size_in_gb['size'], size_in_gb['type']
-        else
-          _thread_for_create_volume hd_name, size_in_gb
-        end
-      end
-
-      threads.each(&:join)
-      threads.collect(&:value)
-    end
-
-    def _thread_for_create_volume(*args)
-      Thread.new do
-        _create_volume(*args)
-      end
-    end
-
-    def _create_volume(hd_name, size_in_gb, type='HDD')
-      name = "#{server_name}_#{hd_name}"
-      log_message = "Create Volume '#{name}' size: #{size_in_gb} GB"
-      options = { :name => name, :size => size_in_gb, :type => type } # type SSD
-      
-      if hd_name == 'root'
-        log_message = "#{log_message}\nBased on #{boot_image.name}"
-        options[:image] = boot_image.id
-        
-        if boot_image.public
-          options[:imagePassword] = root_password
-          options[:sshKeys] = [ssh_key]
-        end
-      else
-        options[:licenceType] = 'OTHER' 
-      end
-
-      volume = dc.create_volume(options)
-      
-      volume.wait_for { ready? }
-      log "#{log_message}\nVolume '#{name}' (#{type}) created\n\n"
-
-      volume
-    rescue => e
-      log "#{log_message}: Error\n\n"
-      raise e
-    end
-
     def do_create_server
       ram_in_gb = server_config['ram_in_gb']
       ram = ram_in_gb * 1024
@@ -70,8 +19,10 @@ module KnifeProfitbricks
       
       log "Create server '#{server_name}': #{ram_in_gb} GB - #{cores} Cores (#{cpu})"
       
+      volumes = build_params_for_volumes
       server = dc.create_server :cores => cores, :ram => ram, :name => server_name,
-        :cpuFamily => cpu
+        :cpuFamily => cpu, :volumes => volumes
+
       server.wait_for { ready? }
       
       add_nic_to_server server
@@ -124,33 +75,45 @@ module KnifeProfitbricks
       end
     end
 
-    def attach_volumes_to_server(volumes)
-      volumes.each do |volume|
-        log "Attach volume #{volume.name} to server #{server.name}"
-        volume.attach(server.id)
-
-        volume.wait_for { ready? }
-        volume.wait_for { volume.reload; !device_number.nil? }
-        
-        log "Volume #{volume.name} attached at device_number #{volume.device_number}"
-        log ''
+    def build_params_for_volumes
+      unless configured_volumes = server_config['volumes']
+        error("No volumes specified! Please specify \"profitbricks\": {\"server\": \"volumes\": {\"root\": SIZE_IN_GB}} in your node!")
       end
-      
-      server.reload
+
+      configured_volumes.map do |hd_name, size_in_gb|
+        if size_in_gb.is_a? Hash
+          build_params_for_volume hd_name, size_in_gb['size'], size_in_gb['type']
+        else
+          build_params_for_volume hd_name, size_in_gb
+        end
+      end
     end
 
-    def set_boot_volume_to_server(boot_volume)
-      log "Set boot volume: #{boot_volume.name}"
-
-      server.update :bootVolume => {:id => boot_volume.id, 
-        :type => 'volume', :href => boot_volume.href}
-
-      server.wait_for { ready? }
-      server.reload
-      boot_volume.reload
+    def build_params_for_volume(hd_name, size_in_gb, type='HDD')
+      name = "#{server_name}_#{hd_name}"
+      log_message = "Volume '#{hd_name}' (#{type}) size: #{size_in_gb} GB"
+      options = { :name => name, :size => size_in_gb, :type => type }
       
-      log "Volume #{boot_volume.name} is used as boot volume!"
-      log ''
+      if hd_name == 'root'
+        log_message = "#{log_message} - Based on #{boot_image.name}"
+        options[:image] = boot_image.id
+        options[:bootOrder] = 'PRIMARY'
+        
+        if boot_image.public
+          options[:imagePassword] = root_password
+          options[:sshKeys] = [ssh_key]
+        end
+      else
+        options[:licenceType] = 'OTHER'
+        options[:bootOrder] = 'NONE'
+      end
+
+      log "  * #{log_message}\n"
+
+      options
+    rescue => e
+      log "#{log_message}: Error\n\n"
+      raise e
     end
 
     def create_server
@@ -158,11 +121,7 @@ module KnifeProfitbricks
       log "Create Server #{server_name.inspect}"
       log ''
       
-      volumes = create_volumes
-      boot_volume = volumes.detect {|v| v.name.end_with? 'root' }
       @server = server = do_create_server
-      attach_volumes_to_server volumes
-      set_boot_volume_to_server boot_volume
       
       check_server_state!
 
